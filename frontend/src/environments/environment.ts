d42be26f68d76ed75318b1e263555ba52f6b91e8
Flores151006 <alejandrofloresmendez2006@gmail.com>
2026-05-07 13:21:02 +0200

    Añadir la URL para desplegar

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production:       false,
-    apiUrl:           'http://localhost:4000/api',
+    apiUrl:           'https://secret-barber-api.onrender.com/api',
     // Google reCAPTCHA v2 — claves de prueba oficiales de Google (siempre válidas)
     recaptchaSiteKey: '6Ldfnd0sAAAAAN9J73arWSPFEJ12qv7ZVvLgpoyy'
 };
